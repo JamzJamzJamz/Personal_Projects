b6ee1ef6223e155d26a84ed12f94c8429a076cf2
James Laptop <le.toanjames@gmail.com>
2019-02-22 15:06:06 -0600

    Initial Youtube commit

---
+++ yt_convert.py
@@ -0,0 +1,22 @@
+import youtube_dl
+import ffmpeg
+
+url = input('Url of youtube video: ')
+title = input('Title of Video: ')
+
+ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+
+ydl_opt = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192', 
+    }]
+}
+
+ydl = youtube_dl.YoutubeDL(ydl_opt)
+
+ydl.download(url)
+
+print('Video has been downloaded')
